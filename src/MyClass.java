21909bf42e9f493593ffaaa5cc288ef4f474a832
ghadeerawaidi <ghadeerfarah@yahoo.com>
2023-04-06 04:35:15 -0800

    second

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 String myName ="ghadeer " ;
-int myPhoneCode = 111;
+int myPhoneCode = 2222;
 System.err.println(myName);
 	}
 
